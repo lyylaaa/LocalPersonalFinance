560d10b5084b4adcd7492ab2f7e172668edbc44c
lyylaaa <nicolemanuele@Nicols-Air.Home>
2023-12-21 16:05:13 +0100

    remove target

--- src/main/java/nixkita/finance/Main.java
+++ src/main/java/nixkita/finance/Main.java
@@ -2,6 +2,6 @@ package nixkita.finance;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao");
+        System.out.println("cifdsfsdfsdao");
     }
 }
