4fbb42417b0103075ea2d39009cb740f8e717f6c
nixkita <nicolemanuele@Nicols-Air.Home>
2023-12-21 16:10:28 +0100

    remove target

--- src/main/java/nixkita/finance/Main.java
+++ src/main/java/nixkita/finance/Main.java
@@ -2,6 +2,6 @@ package nixkita.finance;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("cifdsfsdfsdao");
+        System.out.println("fdsfsdf");
     }
 }
